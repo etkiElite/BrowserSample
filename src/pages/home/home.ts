10a3d0851ea87a5b8c92e3e572938a31b26d5f6a
Julio Valdes <jcvvdf@gmail.com>
2018-10-24 18:52:54 -0400

    adding all files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from "@ionic-native/in-app-browser";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  url: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private inAppBrowser: InAppBrowser) { }
 
+  openWebpage(url: string) {
+    const options: InAppBrowserOptions = {
+      zoom: 'no'
+    }
+
+    // Opening a URL and returning an InAppBrowserObject
+    const browser = this.inAppBrowser.create(url, '_blank', options);
+
+    // Inject scripts, css and more with browser.X
   }
-
 }
